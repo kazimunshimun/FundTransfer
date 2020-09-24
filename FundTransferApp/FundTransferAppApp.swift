dde5d543cb6df20ea9592c355f5578384ba006e4
kazimunshimun <anik1.bd@gmail.com>
2020-09-24 11:24:52 +0600

    Fund transfer app commit

---
+++ FundTransferApp/FundTransferAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundTransferAppApp.swift
+//  FundTransferApp
+//
+//  Created by Anik on 26/8/20.
+//
+
+import SwiftUI
+
+@main
+struct FundTransferAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
